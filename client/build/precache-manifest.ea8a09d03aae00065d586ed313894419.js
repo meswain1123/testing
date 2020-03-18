a33b5127bf995b79f4607e58be144d5646759f55
meswain1123 <meswain@gmail.com>
2020-03-17 19:31:48 -0600

    Added /build to git

---
+++ client/build/precache-manifest.ea8a09d03aae00065d586ed313894419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe63aa77e229bc2b5340a3ec1fc0107b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c14ade4673abbcea0328",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6596a474f480a2d168aa",
+    "url": "/static/js/2.1d70bcbc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1d70bcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14ade4673abbcea0328",
+    "url": "/static/js/main.77236b5d.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
